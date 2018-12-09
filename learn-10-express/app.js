1ffce0a2896e56928d608e37ab004a8a43681cfc
Kevin Chi <kevinchi118@gmail.com>
2018-12-09 18:44:06 -0500

    get with params and query

--- learn-10-express/app.js
+++ learn-10-express/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+// hello world
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
-
 app.listen(3000)
+
+app.get('/example', (req, res) => {
+  res.send('hitting example route');
+})
+
+app.get('/example/:name/:age', (req, res) => {
+  console.log(req.params);
+  console.log(req.query); // ?name=value&name=value in the url
+  res.send(req.params.name + ', age: ' + req.params.age);
+})
