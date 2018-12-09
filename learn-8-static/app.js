f3a44a7377e3544a5e7eeceed589de4a6607cac5
Kevin Chi <kevinchi118@gmail.com>
2018-12-09 13:36:14 -0500

    finishes learn-8-static

--- learn-8-static/app.js
+++ learn-8-static/app.js
@@ -1,2 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+
+http.createServer((req, res) => {
+  const readStream = fs.createReadStream('./index.html');
+  // create header, (status code, content type)
+  res.writeHead(200, {'Content-type': 'text/html'})
+  // response is also a writable stream
+  readStream.pipe(res);
+}).listen(3000);
