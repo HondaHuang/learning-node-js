482408a5df996a04c0716d0d6606d38f107cb386
Kevin Chi <kevinchi118@gmail.com>
2018-12-09 13:28:28 -0500

    finishes learn-7-http

--- learn-7-http/app.js
+++ learn-7-http/app.js
@@ -1,4 +1,13 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
-  
+  if (request.url === '/') {
+    response.write('Hello World from nodejs'); // creates response
+    response.end(); // sends the response
+  } else {
+    response.write('Go to root url');
+    response.end();
+  }
 });
+
+// port
+server.listen('3000')
