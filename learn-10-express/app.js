d90f518e5b07c9eee169303563f5a68944e5c9d2
Kevin Chi <kevinchi118@gmail.com>
2018-12-09 23:27:02 -0500

    validates form data with Joi

--- learn-10-express/app.js
+++ learn-10-express/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 
 // use (alias, middleware)
 app.use('public', express.static(path.join(__dirname, 'static')))
@@ -21,8 +22,24 @@ app.get('/', (req, res) => {
 // allows us to parse json
 app.use(bodyParser.json());
 app.post('/', (req, res) => {
-  console.log(req.body);
-  res.json({success: true});
+  console.log('request: ', req.body)
+  // Joi is an input/data validator
+  const schema = Joi.object().keys({
+    email: Joi.string().trim().email().required(), // requires valid emails
+    // password between 5-10 characters
+    password: Joi.string().min(5).max(10).required()
+  })
+  Joi.validate(req.body, schema, (err, result) => {
+    console.log('result: ', result);
+    console.log('error: ', err);
+    if (err) {
+      console.log(err);
+      res.send('an error has occured');
+    } else {
+      console.log(result);
+      res.send('successfully posted data');
+    }
+  })
 })
 
 app.use(bodyParser.json());
