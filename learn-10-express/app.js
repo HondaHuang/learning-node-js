9041c4064136fad72c5fcfffbde17d9b3b29ec0f
Kevin Chi <kevinchi118@gmail.com>
2018-12-09 19:20:09 -0500

    learns post request with form

--- learn-10-express/app.js
+++ learn-10-express/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// hello world
+// use (alias, middleware)
+app.use('public', express.static(path.join(__dirname, 'static')))
+app.listen(3000)
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
+
+// allows us to parse url encoded forms
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.post('/', (req, res) => {
+//   console.log(req.body);
+//   res.send('successfully posted data');
+// })
+
+// allows us to parse json
+app.use(bodyParser.json());
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json({success: true});
+})
+
+app.use(bodyParser.json());
+app.get('/hello', (req, res) => {
   res.send('Hello World');
 })
-app.listen(3000)
 
 app.get('/example', (req, res) => {
   res.send('hitting example route');
